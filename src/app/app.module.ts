6f0a9bdbf238520ed89723cc4a51b94f1218f4e2
CraigRonald555 <craigronald517@gmail.com>
2021-01-23 02:54:44 +0000

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { OwlModule } from 'ngx-owl-carousel';
+
+const routes = [
+  {path: '', component: AppComponent }
+]
 
 @NgModule({
   declarations: [
@@ -11,10 +16,14 @@ import { OwlModule } from 'ngx-owl-carousel';
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes, {scrollPositionRestoration: 'enabled'}),
     NgbModule,
     OwlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
